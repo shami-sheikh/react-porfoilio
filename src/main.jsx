c1a0834da1233a5737d1c39e3f90b7001d81d5db
shami-sheikh <your-github-samidev.co@gmail.com>
2025-11-25 19:29:58 +0530

    new commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,33 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {Toaster} from 'react-hot-toast'
+import { Toaster } from 'react-hot-toast'
+
+// apna image import karo (Vite isko auto public URL bana dega)
+import favicon from './assets/sami.jpg'
+
+// favicon set karne ka helper
+function setFavicon(url) {
+  try {
+    let link = document.querySelector("link[rel='icon']")
+    if (!link) {
+      link = document.createElement('link')
+      link.rel = 'icon'
+      document.head.appendChild(link)
+    }
+    link.href = url
+  } catch (err) {
+    // ignore (server-side render ya error me)
+  }
+}
+
+// actual call
+setFavicon(favicon)
+
+// render
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-      <Toaster position="top-right" />
+    <Toaster position="top-right" />
   </StrictMode>,
 )
